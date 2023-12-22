d30343ad72929f29f1b86f88cdd359dd7b42a930
Jayparmar123 <jayparmar.studenttdb@gmail.com>
2023-12-22 18:54:52 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,25 @@
+t=(0,23,45,15,-15,78)
+# print(t)
+# print(t[-2:-5:-1])
+# print(t[-1:-6:-2])
+# print(t[0:6:2])
+
+# a=t[0]
+
+# for i in t:
+#     if i<a:
+#         a=i
+# print(a)
+
+
+# tuple 
+# user Value
+
+# sum
+# average
+
+t=input("enter number:")
+a=(0,)
+for i in t:
+    if i
+print(a)
